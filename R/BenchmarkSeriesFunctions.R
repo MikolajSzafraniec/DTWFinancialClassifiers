94af865d8f367e197b971c4dd4221f51a5358f43
MikolajSzafraniec <methodos77@gmail.com>
2020-05-13 20:42:36 +0200

    Added functions conected with benchmark time series processing

--- R/BenchmarkSeriesFunctions.R
+++ R/BenchmarkSeriesFunctions.R
@@ -1,4 +1,50 @@
-load_benchmark_series <- function(benchmarkDataPath, dimNumbers = NULL){
+load_benchmark_series_MD_dataset <- function(filesPaths, classIndicatorCol = NULL){
+  
+  dataList <- purrr::map(filesPaths, .f = farff::readARFF)
+  nDim <- length(dataList)
+  
+  if(is.null(classIndicatorCol)){
+    ncols <- purrr::map_int(dataList, ncol)
+    
+    if(!all(ncols == ncols[1]))
+      stop("Number of columns among listed files is not equal.")
+    
+    classIndicatorCol <- ncols[1]
+  }
+  
+  classColName <- colnames(dataList[[1]])[classIndicatorCol]
+  
+  dataGathered <- purrr::imap_dfr(dataList, .f = function(x, i, classColName){
+    
+    res <- x %>%
+      mutate(dimnum = paste("dim", i, sep = "_"), tsNum = 1:nrow(x)) %>%
+      tidyr::pivot_longer(cols = -c(classColName, "dimnum", "tsNum"),
+                          names_to = "ObsNum", 
+                          values_to = "TS_val") %>%
+      mutate(ObsNum = as.numeric(stringr::str_match(ObsNum,
+                                         "(?<=channel_[0-9]{1,3}_).+")) + 1)
+      
+    
+    return(res)
+    
+  }, classColName = classColName)
+  
+  res <- dataGathered %>%
+    tidyr::pivot_wider(names_from = dimnum,
+                       values_from = TS_val) %>%
+    tidyr::nest(TS = -c(classColName, "tsNum")) %>%
+    mutate(TS = purrr::map(TS, .f = function(x){
+      x %>%
+        dplyr::arrange(ObsNum) %>%
+        dplyr::select(-ObsNum)
+    })) %>%
+    dplyr::rename("ClassInd" = classColName)
+  
+  return(res)
+}
+
+
+load_benchmark_series_activities <- function(benchmarkDataPath, dimNumbers = NULL){
   
   # Getting list of all files stored in all subdirectories
   filesList <- list.files(benchmarkDataPath, all.files = T, recursive = T)
@@ -35,7 +81,7 @@ load_benchmark_series <- function(benchmarkDataPath, dimNumbers = NULL){
 }
 
 
-findNNBenchmarkSeries <- function(benchmarkTS,
+findNNBenchmarkSeries_activities <- function(benchmarkTS,
                                   refIndex, 
                                   shapeDTWParams,
                                   targetDistance = c("raw", "shape"),
@@ -100,7 +146,7 @@ findNNBenchmarkSeries <- function(benchmarkTS,
   return(res)
 }
 
-benchSeriesSelfClassParallel <- function(benchmarkTS, 
+benchSeriesSelfClassParallel_activities <- function(benchmarkTS, 
                                          shapeDTWParams,
                                          targetDistance = c("raw", "shape"),
                                          distanceType = c("Dependent", "Independent"),
@@ -124,14 +170,14 @@ benchSeriesSelfClassParallel <- function(benchmarkTS,
                     {
                       furrr::future_map_chr(.x = indexesList, 
                                             function(ind){
-                                              class_res <- findNNBenchmarkSeries(benchmarkTS = benchmarkTS, 
+                                              class_res <- findNNBenchmarkSeries_activities(benchmarkTS = benchmarkTS, 
                                                                                  refIndex = ind, 
                                                                                  shapeDTWParams = shapeDTWParams, 
                                                                                  targetDistance = targetDistance, 
                                                                                  distanceType = distanceType, 
                                                                                  normalizationType = normalizationType, 
                                                                                  subsequenceWidth = subsequenceWidth, 
-                                                                                 trigonometricTP = subsequenceWidth)
+                                                                                 trigonometricTP = trigonometricTP)
                                               return(class_res$activitySym)
                                             }, 
                                             .progress = F)
@@ -144,3 +190,125 @@ benchSeriesSelfClassParallel <- function(benchmarkTS,
 }
 
 
+#####################################################################################
+
+findNNBenchmarkSeries_general <- function(benchmarkTS,
+                                          refIndex, 
+                                          shapeDTWParams,
+                                          testSet = NULL,
+                                          excludeCol = NULL,
+                                          targetDistance = c("raw", "shape"),
+                                          distanceType = c("Dependent", "Independent"),
+                                          normalizationType = c("Unitarization", "Zscore"),
+                                          subsequenceWidth = 4,
+                                          trigonometricTP = NULL){
+  
+  message("Looking for the NN for the series: ", refIndex, "/", nrow(benchmarkTS))
+  
+  targetDistance <- match.arg(targetDistance)
+  distanceType <- match.arg(distanceType)
+  normalizationType <- match.arg(normalizationType)
+  
+  if(is.null(testSet)){
+    
+    if(is.null(excludeCol)) stop("Param excludeCol must be specified when testSet is not passed")
+    
+    excludeCol <- dplyr::sym(excludeCol)
+    
+    refObject <- benchmarkTS %>%
+      dplyr::pull(!!excludeCol) %>%
+      .[refIndex]
+    
+    testSet <- benchmarkTS %>%
+      dplyr::filter(!!excludeCol != refObject)
+  }
+  
+  referenceTibble <- benchmarkTS %>%
+    dplyr::pull(TS) %>%
+    .[refIndex]
+  
+  results_set <- purrr::pmap(.l = list(refTS = referenceTibble,
+                                       testTS = testSet$TS), 
+                             .f = function(refTS, testTS){
+                               refTS <- as.matrix(refTS)
+                               testTS <- as.matrix(testTS)
+                               
+                               res <- RcppShapeDTW::kNNShapeDTWCpp(referenceSeries = refTS, 
+                                                                   testSeries = testTS, 
+                                                                   forecastHorizon = 0, 
+                                                                   subsequenceWidth = subsequenceWidth, 
+                                                                   subsequenceBreaks = 1, 
+                                                                   shapeDescriptorParams = shapeDTWParams, 
+                                                                   normalizationType = normalizationType, 
+                                                                   distanceType = distanceType)
+                             })
+  
+  apply_at <- ifelse(targetDistance == "raw", 
+                     "RawSeriesDistanceResults",
+                     "ShapeDescriptorsDistanceResults")
+  
+  distances <- map_depth(.x = results_set, .depth = 1, .f = function(x, td, apply_at){
+    
+    if(targetDistance == "raw"){
+      return(x[[apply_at]]$RawDistance)
+    }else{
+      return(x[[apply_at]]$ShapeDescriptorsDistance)
+    }
+    
+  }, td = targetDistance, apply_at = apply_at)
+  
+  which_dist_min <- which.min(distances)
+  
+  res <- testSet %>%
+    dplyr::select(ClassInd, tsNum) %>%
+    .[which_dist_min,]
+  
+  return(res)
+}
+
+benchSeriesSelfClassParallel_general <- function(benchmarkTS, 
+                                                 shapeDTWParams,
+                                                 testSet = NULL,
+                                                 excludeCol = NULL,
+                                                 targetDistance = c("raw", "shape"),
+                                                 distanceType = c("Dependent", "Independent"),
+                                                 normalizationType = c("Unitarization", "Zscore"),
+                                                 subsequenceWidth = 4,
+                                                 trigonometricTP = NULL){
+  
+  targetDistance = match.arg(targetDistance)
+  distanceType = match.arg(distanceType)
+  normalizationType = match.arg(normalizationType)
+  
+  indexesList <- 1:nrow(benchmarkTS)
+  
+  if(class(future::plan())[2] == "sequential"){
+    message("Switching plan to multiprocess.")
+    future::plan(future::multiprocess)
+  }
+  
+  resTS <- benchmarkTS %>%
+    dplyr::mutate(classificationResults = 
+    {
+      furrr::future_map_chr(.x = indexesList, 
+                            function(ind){
+                              class_res <- findNNBenchmarkSeries_general(benchmarkTS = benchmarkTS, 
+                                                                 refIndex = ind,
+                                                                 testSet = testSet,
+                                                                 excludeCol = excludeCol,
+                                                                 shapeDTWParams = shapeDTWParams, 
+                                                                 targetDistance = targetDistance, 
+                                                                 distanceType = distanceType, 
+                                                                 normalizationType = normalizationType, 
+                                                                 subsequenceWidth = subsequenceWidth, 
+                                                                 trigonometricTP = trigonometricTP)
+                              return(class_res$ClassInd)
+                            }, 
+                            .progress = T)
+    })
+  
+  message("Switching plan to sequential.")
+  future::plan(future::sequential)
+  
+  return(resTS)
+}
